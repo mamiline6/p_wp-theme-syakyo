fc14c3d6d59fc2147dcce45765af4cc927da59c9
mamiline6 <ohmamirineoo6@gmail.com>
2020-09-13 09:20:26 +0900

    #4 投稿記事のループ処理・表示部分を抽出
    条件分岐の書き方が複数ある（全文にphpをつける、phpで囲む、：で分ける）それぞれをスニペット化する
    html を挟む書き方を調べる

--- index.php
+++ index.php
@@ -1,62 +1,24 @@
+<?php
+get_header();
+?>
+
 <main id="site-content" role="main">
 
-    <?php
+  <?php 
 
-    $archive_title    = '';
-    $archive_subtitle = '';
+  if ( have_posts() ) {
 
-    if ( is_search() ) {
-      global $wp_query;
+    while ( have_posts() ) {
 
-      $archive_title = sprintf(
-        '%1$s %2$s',
-        '<span class="color-acccent">' . __( 'Search', 'twentytwenty' ) . '</span>',
-        '&ldquo;' . get_search_query() . '&rdquo;'
-      );
-    }
+      the_post();
+      the_title();
+    } 
 
-    if ( have_post() ) {
+  }
+  ?>
 
-      $i = 0;
-
-      while ( have_post() ) {
-        $i++;
-        if ( $i > 1 ) {
-          echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
-        }
-        the_post();
-        echo '投稿が掲載されますよ';
-
-        get_template_part( 'template-parts/content', get_post_type() );
-
-      }
-    } elseif ( is_search() ) {
-      ?>
-
-      <div class="no-search-results-form section-inner thin">
-
-        <?php
-        get_search_form(
-          array(
-            'label' => __( 'search again', twentytwenty ),
-          )
-        );
-        ?>
-      
-      </div><!-- .no-search-results -->
-
-      <?php
-    } else {
-      echo '投稿はありませんよ';
-    }
-
-    ?>
-
-    <?php get_template_part( 'template-parts/pagetation' ); ?>
 
 </main><!-- #site-content -->
-
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
 
 <?php
 get_footer();
